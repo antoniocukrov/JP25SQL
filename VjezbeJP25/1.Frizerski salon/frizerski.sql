bbe64941897c2a0df5a7c6426824d5ba35a2ada3
antoniocukrov <antonio.cukrov1@gmail.com>
2021-11-18 15:02:41 +0100

    Slike+skriptajp25
    Nema jos inserta

--- VjezbeJP25/1.Frizerski salon/frizerski.sql
+++ VjezbeJP25/1.Frizerski salon/frizerski.sql
@@ -3,7 +3,7 @@ create database frizerski character set utf8;
 use frizerski;
 
 # otvoriti cmd i zaljepiti od znaka # do kraja - pripaziti na putanju
-# c:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < c:\frizerski.sql
+# c:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < C:\Users\Lord Pupcent\Documents\GitHub\JP25SQL\VjezbeJP25\1.Frizerski salon\frizerski.sql
 
 create table djelatnica(
     sifra int not null primary key auto_increment,
@@ -33,8 +33,19 @@ create table posjet(
     usluga int not null  
 );
 
+create table osoba(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11)
+);
+
+
+
 alter table posjet add foreign key (korisnik) references korisnik(sifra);
 alter table posjet add foreign key (usluga) references usluga(sifra);
 
 alter table djelatnica add foreign key (posjet) references posjet(sifra);
 
+alter table djelatnica add foreign key (osoba) references osoba(sifra);
+alter table korisnik add foreign key (osoba) references osoba(sifra);
